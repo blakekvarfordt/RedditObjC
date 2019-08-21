f9e83d6012ea2b36da6b4f31890a82b2aeae6a03
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-21 15:46:41 -0600

    sets up url and fetching in the model controller

---
+++ RedditObjC/RedditObjC/ModelController/BTKPostController.h
@@ -0,0 +1,23 @@
+//
+//  BTKPostController.h
+//  RedditObjC
+//
+//  Created by Blake kvarfordt on 8/21/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// This helps with build time. Not App Performance
+@class BTKPost;
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BTKPostController : NSObject
+
++ (instancetype)sharedController;
+
+-(void) searchForPostWithSearchTerm:(NSString *)searchTerm completion: (void(^) (NSArray<BTKPost *> *posts, NSError *error))completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RedditObjC/RedditObjC/ModelController/BTKPostController.m
@@ -0,0 +1,89 @@
+//
+//  BTKPostController.m
+//  RedditObjC
+//
+//  Created by Blake kvarfordt on 8/21/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+#import "BTKPostController.h"
+#import "BTKPost.h"
+
+
+
+@implementation BTKPostController
+
+
+// Shared Instance
++ (BTKPostController *) shared {
+    static BTKPostController *shared = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shared = [BTKPostController new];
+    });
+    return shared;
+}
+
+// Reddits URL is built with /components and .extensions
++ (NSURL *)baseURL;
+{
+    return [NSURL URLWithString:@"https://www.reddit.com/r"];
+}
+
+- (void)searchForPostWithSearchTerm:(NSString *)searchTerm completion:(void (^)(NSArray<BTKPost *> *, NSError *))completion
+{
+    // Build URL
+    NSURL *url = [[BTKPostController baseURL]URLByAppendingPathComponent:searchTerm];
+    
+    NSURL *finalURL = [url URLByAppendingPathExtension:@"json"];
+    
+    // NSURLSession
+    [[NSURLSession.sharedSession dataTaskWithURL:finalURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        // You could show the error to the user if you wanted
+        if (error) {
+            NSLog(@"ERROR: %@", error);
+            return completion(nil, [NSError errorWithDomain:@"Error Fetching json" code:(NSInteger)-1 userInfo:nil]);
+        }
+        
+        // This is just for the developer
+        if (!data) {
+            NSLog(@"ERROR no data returned from the dataTask");
+            return completion(nil, [NSError errorWithDomain:@"no data returned" code:(NSInteger)-1 userInfo:nil]);
+        }
+        
+        
+        
+        // Parse the data that comes beck from data task
+        NSDictionary *topLevelDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error: &error];
+        NSDictionary *dataDict = topLevelDict[@"data"];
+        NSArray *childrenArray = dataDict[@"children"];
+        
+        // Array Placeholder
+        NSMutableArray *postArray = [NSMutableArray new];
+        
+        for (NSDictionary *postDataDict in childrenArray)
+        {
+            // initialize our object
+            BTKPost *post = [[BTKPost alloc] initWithDictionary:postDataDict];
+            
+            [postArray addObject:post];
+            
+            // Call completion
+            completion(postArray, nil);
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }]resume];
+    
+    
+}
+
+
+@end
